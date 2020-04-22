4f0e42f01c97a723128730a6af651e26cde6e87e
bhanvigoel <62538482+bhanvigoel@users.noreply.github.com>
2020-04-22 14:01:48 +0530

    updated assignments

--- src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
+++ src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+imports:[CommonModule]
+import { CommonModule } from '@angular/common';
+import { Todo, AssignmentComponent } from './../../assignment.component';
 
 @Component({
   selector: 'app-assignment2',
@@ -7,4 +10,13 @@ import { Component } from '@angular/core';
 })
 export class Assignment2Component {
 
+  
+  selectedRow:Number
+    
+  
+  mylist=new AssignmentComponent
+  todos:Todo[]=this.mylist.todos
+  getClickedRow :Function = function(index){
+    this.selectedRow = index; }
+  
 }
